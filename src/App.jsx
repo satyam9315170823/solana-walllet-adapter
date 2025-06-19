96525bf8e9fc86f851820473d32f3c0698c106e3
satyam9315170823 <131178374+satyam9315170823@users.noreply.github.com>
2025-06-19 11:01:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,9 +21,8 @@ const App = () => {
         <WalletModalProvider>
           <div className="flex flex-col items-center space-y-5 p-6 bg-gray-100 rounded-lg shadow-lg">
             <h1 className="font-mono font-bold text-4xl  mb-6 text-gray-700 ">DappNation</h1>
-          <WalletMultiButton>
-            Connect to wallet
-          </WalletMultiButton>
+          <WalletMultiButton />
+          
             <Airdrop />
             <WalletDisconnectButton>disconnect wallet</WalletDisconnectButton>
           </div>
